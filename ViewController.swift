373c318f43d3e94e559ba99b7cfc06f7f640d332
emirhan acısu <emirhan.acisu@gmail.com>
2019-09-28 23:09:46 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Erhan Acisu on 28.09.2019.
+//  Copyright © 2019 Emirhan Acisu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
